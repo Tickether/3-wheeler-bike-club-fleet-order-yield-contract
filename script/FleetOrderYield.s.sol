aebb0d9df380e8a54049f849a1bd912dd6d756cf
Tickether <tickether@gmail.com>
2025-05-08 10:48:15 +0000

    chore: cleanup & base code

--- script/FleetOrderYield.s.sol
+++ script/FleetOrderYield.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {FleetOrderYield} from "../src/FleetOrderYield.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract FleetOrderYieldScript is Script {
+    FleetOrderYield public fleetOrderYield;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        fleetOrderYield = new FleetOrderYield();
 
         vm.stopBroadcast();
     }
